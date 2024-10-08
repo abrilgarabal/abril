8817d4f9d494c475c8998f42fcd35c3e1eb6b42d
abrilgarabal <a.abrilgarabal@icea.edu.ar>
2024-10-08 19:11:29 +0000

    g

--- hola.py
+++ hola.py
@@ -1,1 +1,16 @@
-q
+lista=('abril', 'garabal', 'ornella',)
+ombre=lista(1)
+apellido=(2)
+string=('hola')
+numeros=(233+3)-2+5
+numeros=223
+booleanos = (True)
+booleanos = (False, True)
+booleanos=True
+if booleanos:
+    print (23)
+    it= 'pennywise'
+booleanos=(False or True)
+
+
+ 
